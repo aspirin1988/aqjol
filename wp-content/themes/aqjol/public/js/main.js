ea1411e6d629e6a191201a8bbf66222f8f391310
Sergey <aspirin_1988@mail.ru>
2016-04-21 13:44:50 +0600

    /start

--- wp-content/themes/aqjol/public/js/main.js
+++ wp-content/themes/aqjol/public/js/main.js
@@ -11,11 +11,12 @@ $('#pref').click(function() {
     var max_img = gall.find('img').length;
     if (current === 0) {
         var src = gall.find('[data-id="' + (max_img - 1) + '"]').attr('src');
-        img.attr('src', src).data('current', (max_img - 1));
+        slide(img,src,500,(max_img - 1));
     }
     else {
         var pref = gall.find('[data-id="' + (current - 1) + '"]').attr('src');
-        img.attr('src', pref).data('current', (current - 1));
+        slide(img,pref,500,(current - 1));
+
     }
     return false;
 });
@@ -26,11 +27,23 @@ $('#next').click(function() {
     var max_img = gall.find('img').length;
     if (current === (max_img - 1)) {
         var src = gall.find('[data-id="' + 0 + '"]').attr('src');
-        img.attr('src', src).data('current', 0);
+        slide(img,src,500,0);
     }
     else {
         var pref = gall.find('[data-id="' + (current + 1) + '"]').attr('src');
-        img.attr('src', pref).data('current', (current + 1));
+        slide(img,pref,500,(current + 1));
+
     }
     return false;
 });
+
+function slide(obj,src,fade,surrent)
+{
+    $('.modal-body').slideToggle(fade).after(function(){
+          setTimeout(function(){obj.attr('src', src).data('current', surrent);
+            $('.modal-body').slideToggle(fade)},fade);
+
+    });
+
+
+}
